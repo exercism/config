a37cb2c0391c4780caaa4fba13d2036cf4f23153
Jeremy Walker <jez.walker@gmail.com>
2020-08-29 20:30:31 +0100

    Respect Rails.env if present

--- lib/exercism_config/determine_environment.rb
+++ lib/exercism_config/determine_environment.rb
@@ -4,6 +4,10 @@ module ExercismConfig
 
     def call
       env = ENV['EXERCISM_ENV'] || ENV['RAILS_ENV'] || ENV['APP_ENV']
+      unless env
+        env = Rails.env.to_s if Object.const_defined?(:Rails) && Rails.respond_to?(:env)
+      end
+
       raise Exercism::ConfigError, 'No environment set - set one of EXERCISM_ENV, RAILS_ENV or APP_ENV' unless env
 
       Environment.new(env)
