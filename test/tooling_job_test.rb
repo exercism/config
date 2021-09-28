bd560533b3c9f9d1df6d069400cc3d4a1b15f3fb
Jeremy Walker <jez.walker@gmail.com>
2021-09-28 14:43:34 +0100

    Refactor to store metadata in s3 (#30)
    * Bring storing of files into config
    
    * Remove jobs after processing

--- test/tooling_job_test.rb
+++ test/tooling_job_test.rb
@@ -22,26 +22,22 @@ module Exercism
       assert_equal 'bar-2', job.foo
     end
 
-    def test_retrieves_metadata_from_s3 # rubocop:disable Naming/VariableNumber
-      job_id = SecureRandom.uuid
-      stdout = "Some output"
-      stderr = "Some errors"
+    def test_queues_job
+      redis = Exercism.redis_tooling_client
+      submission_uuid = SecureRandom.uuid
+      type = :test_runner
 
-      bucket = Exercism.config.aws_tooling_jobs_bucket
-      folder = "#{Exercism.env}/#{job_id}"
-      upload_to_s3(bucket, "#{folder}/stdout", stdout)
-      upload_to_s3(bucket, "#{folder}/stderr", stderr)
+      job = ToolingJob.create!(type, submission_uuid, :ruby, "two-fer")
 
-      job = ToolingJob.new(job_id, {})
-      assert_equal stdout, job.stdout
-      assert_equal stderr, job.stderr
+      assert redis.get("submission:#{submission_uuid}:#{type}")
+      assert redis.get("job:#{job.id}")
     end
 
     def test_cancels_test_runner_job
       redis = Exercism.redis_tooling_client
       submission_uuid = SecureRandom.uuid
 
-      job = ToolingJob.create!(submission_uuid, :test_runner, :ruby, "two-fer")
+      job = ToolingJob.create!(:test_runner, submission_uuid, :ruby, "two-fer")
       job.cancelled!
 
       assert_nil redis.lindex(ToolingJob.key_for_queued, 0)
@@ -52,7 +48,7 @@ module Exercism
       redis = Exercism.redis_tooling_client
       submission_uuid = SecureRandom.uuid
 
-      job = ToolingJob.create!(submission_uuid, :test_runner, :ruby, "two-fer")
+      job = ToolingJob.create!(:test_runner, submission_uuid, :ruby, "two-fer")
       job.locked!
 
       assert_nil redis.lindex(ToolingJob.key_for_queued, 0)
@@ -64,11 +60,10 @@ module Exercism
       submission_uuid = SecureRandom.uuid
       status = "foobar"
       output = "say what now"
-      exception = { 'oh' => 'no' }
 
-      job = ToolingJob.create!(submission_uuid, :test_runner, :ruby, "two-fer")
+      job = ToolingJob.create!(:test_runner, submission_uuid, :ruby, "two-fer")
       job.locked!
-      job.executed!(status, output, exception)
+      job.executed!(status, output)
 
       assert_nil redis.lindex(ToolingJob.key_for_queued, 0)
       assert_nil redis.lindex(ToolingJob.key_for_locked, 0)
@@ -77,22 +72,51 @@ module Exercism
       job = ToolingJob.find(job.id)
       assert_equal status, job.execution_status
       assert_equal output, job.execution_output
-      assert_equal exception, job.execution_exception
     end
 
     def test_marks_job_as_processed
       redis = Exercism.redis_tooling_client
       submission_uuid = SecureRandom.uuid
+      type = :test_runner
 
-      job = ToolingJob.create!(submission_uuid, :test_runner, :ruby, "two-fer")
+      job = ToolingJob.create!(type, submission_uuid, :ruby, "two-fer")
+
+      assert redis.get("submission:#{submission_uuid}:#{type}")
+      assert redis.get("job:#{job.id}")
       job.locked!
-      job.executed!(nil, nil, nil)
+      job.executed!(nil, nil)
       job.processed!
 
       assert_nil redis.lindex(ToolingJob.key_for_queued, 0)
       assert_nil redis.lindex(ToolingJob.key_for_locked, 0)
       assert_nil redis.lindex(ToolingJob.key_for_executed, 0)
-      assert_equal job.id, redis.lindex(ToolingJob.key_for_processed, 0)
+
+      assert_nil redis.get("submission:#{submission_uuid}:#{type}")
+      assert_nil redis.get("job:#{job.id}")
+    end
+
+    def test_stores_and_retrieves_stdout
+      stdout = "Some scary stdout"
+      id = SecureRandom.hex
+      job = ToolingJob.new(id, {})
+      job.store_stdout!(stdout)
+      assert_equal stdout, job.stdout
+    end
+
+    def test_stores_and_retrieves_stderr
+      stderr = "Some scary stdout"
+      id = SecureRandom.hex
+      job = ToolingJob.new(id, {})
+      job.store_stderr!(stderr)
+      assert_equal stderr, job.stderr
+    end
+
+    def test_stores_and_retrieves_metadata
+      metadata = { 'foo' => { "bar" => ['cat', 1] } }
+      id = SecureRandom.hex
+      job = ToolingJob.new(id, {})
+      job.store_metadata!(metadata)
+      assert_equal metadata, job.metadata
     end
   end
 end
